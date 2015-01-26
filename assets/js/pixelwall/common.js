4ebe0d866dbc6a7ffd0dc0ab4ebc023abfbf0d44
Michael Fladischer <FladischerMichael@fladi.at>
2015-01-26 12:54:50 +0100

    Replace ui.bootstrap carousel with our own implementation.

--- assets/js/pixelwall/common.js
+++ assets/js/pixelwall/common.js
@@ -538,26 +538,39 @@ angular.module(
     function($compile) {
         return {
             restrict: 'E',
-            replace: true,
             templateUrl: 'assets/templates/pixelwall/box/images.html',
             controller: [
                 '$scope',
                 '$element',
                 '$attrs',
                 '$timeout',
+                '$interval',
                 function (
                     $scope,
                     $element,
                     $attrs,
-                    $timeout
+                    $timeout,
+                    $interval
                 ) {
-                    if ($scope.box.data.images.length) {
+                    $scope.images = $scope.box.data.images.map(function(image) {
+                      return {
+                        src: image
+                      };
+                    });
+                    if (
+                      typeof $scope.box.data.images !== 'undefined' &&
+                      typeof $scope._timeout !== 'undefined'
+                    ) {
                         $timeout.cancel($scope._timeout);
                         var resolver = $timeout(function() {
                             console.log("All slides played!");
                             $scope.box._defer.resolve();
                         }, $scope.box.data.images.length * $scope.box.data.interval * 1000);
                     }
+                    $scope.$watch('box.data.interval', function(newVal) {
+                      console.log('Interval: ', newVal);
+                    });
+
                 }
             ]
         };
