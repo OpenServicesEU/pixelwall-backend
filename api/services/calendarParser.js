6100a6190326986cf0d9759772b72f1389c0e4a7
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-01-29 22:41:40 +0100

    Only store events received from an ical-url which have an end date of today or later.

--- api/services/calendarParser.js
+++ api/services/calendarParser.js
@@ -6,13 +6,20 @@ module.exports = {
         ical.fromURL(url, "", function(err, data) {
 
             var events = [];
+            var today = new Date();
+            today.setHours(0);
+            today.setMinutes(0);
+            today.setSeconds(0);
+            today.setMilliseconds(0);
 
             for (var k in data) {
                 if (!data.hasOwnProperty(k))
                     continue;
                 
                 var event = data[k];
-                events.push(event);
+                
+                if (event.end > today)
+                    events.push(event);
             }
 
             cb(err, events);
