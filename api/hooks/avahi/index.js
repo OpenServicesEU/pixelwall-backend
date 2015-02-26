5b83a6580caf50e8838e5754738596adf62d0fba
Michael Fladischer <FladischerMichael@fladi.at>
2015-02-26 12:43:43 +0100

    Replace internal avahi solution with our newly released sails-generate-avahi.

--- api/hooks/avahi/index.js
+++ api/hooks/avahi/index.js
@@ -5,62 +5,64 @@
 var Enum = require('enum');
 var DBus = require('dbus-native');
 
+// Mimicking the constants from python-avahi library for conventience.
+// Some unused constants are missing.
 var avahi = new Enum({
   'IF_UNSPEC': -1,
   'PROTO_UNSPEC': -1,
   'PROTO_INET': 0,
   'SERVER_RUNNING': 2,
-  'SERVER_COLLISION': 3
+  'SERVER_COLLISION': 3,
+  'DBUS_NAME': 'org.freedesktop.Avahi',
+  'DBUS_PATH_SERVER': '/',
+  'DBUS_INTERFACE_SERVER': 'org.freedesktop.Avahi.Server',
+  'DBUS_INTERFACE_ENTRY_GROUP': 'org.freedesktop.Avahi.EntryGroup'
 });
 
-var defaultConfig = {
-  name: "Sails.js",
-  type: "_sails._tcp",
-  domain: "",
-  host: "",
-  port: 1337
-};
-
 module.exports = function (sails) {
-  process.env.DISPLAY = ':0';
-  process.env.DBUS_SESSION_BUS_ADDRESS = 'unix:path=/run/dbus/system_bus_socket';
+  //process.env.DISPLAY = ':0';
+  //process.env.DBUS_SESSION_BUS_ADDRESS = 'unix:path=/run/dbus/system_bus_socket';
   var bus = DBus.systemBus();
+
+  // Scoped variables to hold the various objects retrieved from dbus.
   var server;
   var service;
   var group;
-  var config = _.merge(defaultConfig, sails.config.avahi);
+  var config;
 
+  // Remove the service after a StateChanged signal if necessary.
   function remove_service() {
     if (group) {
       group.Reset();
     }
   }
+
+  // Add a new EntryGroup for our service.
   function add_group() {
-    if (!group) {
-      server.EntryGroupNew(function(err, path) {
-        if (err) {
-          sails.log.error('DBus: Could not call org.freedesktop.Avahi.Server.EntryGroupNew');
-          return;
+    server.EntryGroupNew(function(err, path) {
+      if (err) {
+        sails.log.error('DBus: Could not call org.freedesktop.Avahi.Server.EntryGroupNew');
+        return;
+      }
+      service.getInterface(
+        path,
+        avahi.DBUS_INTERFACE_ENTRY_GROUP.value,
+        function (
+          err,
+          newGroup
+        ) {
+          group = newGroup;
+          add_service();
         }
-        service.getInterface(
-          path,
-          'org.freedesktop.Avahi.EntryGroup',
-          function (
-            err,
-            newGroup
-          ) {
-            group = newGroup;
-            add_service();
-          }
-        )
-      });
-      server.EntryGroupNew();
-    } else {
-      add_service();
-    }
+      )
+    });
   }
+
+  // Add our service definition.
   function add_service() {
-    sails.log.info('Publishing service ' + config.serviceName + ' (' + config.serviceType + ') on port '+ config.port);
+    // Default configuration. Overrides can be defined in `config/avahi.js`.
+
+    sails.log.info('Publishing service ' + config.name + ' (' + config.type + ') on port '+ config.port);
     group.AddService(
       avahi.IF_UNSPEC.value,
       avahi.PROTO_INET.value,
@@ -80,6 +82,8 @@ module.exports = function (sails) {
       }
     );
   }
+
+  // Handle StateChange signals from the avahi server.
   function stateChanged(state) {
     if (state == avahi.SERVER_COLLISION.value) {
       remove_service();
@@ -90,11 +94,27 @@ module.exports = function (sails) {
   return {
     // Run when sails loads-- be sure and call `next()`.
     initialize: function (next) {
-      sails.log.info('Registering service with avahi over dbus');
-      service = bus.getService('org.freedesktop.Avahi');
+      var defaultConfig = {
+        enabled: true,
+        name: "Sails.js",
+        type: "_sails._tcp",
+        domain: "",
+        host: "",
+        port: sails.config.proxyPort || sails.config.port
+      };
+
+      // Merge default config with custom config.
+      config = _.merge(defaultConfig, sails.config.avahi);
+
+      // See if service publishing is desired
+      if (!config.enabled) {
+        return next();
+      }
+      // Get the avahi service from dbus.
+      service = bus.getService(avahi.DBUS_NAME.value);
       service.getInterface(
-        '/',
-        'org.freedesktop.Avahi.Server',
+        avahi.DBUS_PATH_SERVER.value,
+        avahi.DBUS_INTERFACE_SERVER.value,
         function(
           err,
           newServer
